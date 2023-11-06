002eca0d71dc9d504edcab41886f360c6acd64fd
Powerboy3551 <145390119+Powerboy3551@users.noreply.github.com>
2023-11-06 16:24:02 -0500

    Added a click listener and fixed the url using validateUrl

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -6,6 +6,8 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,11 +24,35 @@ class MainActivity : AppCompatActivity() {
         webView = findViewById(R.id.webView)
 
         // Allow your browser to intercept hyperlink clicks
-        webView.webViewClient = object: WebViewClient() {
+        webView.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
             }
         }
 
+        goButton.setOnClickListener {
+            val enteredUrl = urlEditText.text.toString()
+            val validUrl = validateAndFixUrl(enteredUrl)
+
+            if (validUrl != null) {
+                webView.loadUrl(validUrl)
+            } else {
+                // Display an error message if the URL is not valid
+                Toast.makeText(this, "Invalid URL", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun validateAndFixUrl(url: String): String? {
+        try {
+            val parsedUrl = URL(url)
+            // If the URL doesn't have a scheme (e.g., http:// or https://), add "https://" by default
+            if (parsedUrl.protocol == null) {
+                return "https://$url"
+            }
+            return url
+        } catch (e: Exception) {
+            return null  // URL is not valid
+        }
     }
 }
